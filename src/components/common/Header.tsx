da4c70db056f3a57d372379ea3552a61ceb5b119
Jun_53 <58911113+jun-05@users.noreply.github.com>
2023-11-26 00:03:41 +0900

    fix : 헤드 및 헤더 이름 변경 및 - 사이트 헤드 이름을 special legend로 변경 - 헤더의 이름을 SL로 변경 - 이후 메타데이터 및 아이콘 변경을 위한 _document.tsx 파일 생성

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = () => (
   <header className="p-4 bg-blue-500 text-white dark:bg-blue-800">
-    <h1 className="text-xl hover:cursor-pointer">SL.GG</h1>
+    <h1 className="text-xl hover:cursor-pointer">SL</h1>
   </header>
 );
 
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,18 @@
 import { AppProps } from 'next/app';
 import '../styles/globals.css';
 import Layout from '@/components/common/Layout';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Head>
+        <title>Special Legends</title>
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
 
