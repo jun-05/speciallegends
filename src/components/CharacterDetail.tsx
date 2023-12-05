07e37284f0c0fa3ed46631b13a55c575565b44a5
Jun_53 <58911113+jun-05@users.noreply.github.com>
2023-12-05 20:43:09 +0900

    fix : 이미지 타이틀 넓이수정 - 로컬이 en일때 ability rate 텍스트와 enchantment 텍스트의 높이가   다른현상 수정

--- src/components/CharacterDetail.tsx
+++ src/components/CharacterDetail.tsx
@@ -45,7 +45,7 @@ const CharacterDetail = ({ characterData }: CharacterDetailProps) => {
   return (
     <td colSpan={5} className="bg-gray-300 dark:bg-gray-700 dark:text-white">
       <div className="flex flex-grow w-full justify-around md:space-x-6 space-x-8">
-        <div className="w-36 sm:w-60 md:w-80  py-2 ml-[10%] lg:ml-0">
+        <div className="w-32 sm:w-60 md:w-80  py-2 ml-[10%] lg:ml-0">
           <h2 className="text-sm font-bold mb-2 sm:min-h-0">
             {languageTranslations.firstInfoHeadName}
           </h2>
