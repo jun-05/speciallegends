9798216b8ecd328492d92083f5a88c4d79243892
Jun_53 <58911113+jun-05@users.noreply.github.com>
2023-12-05 20:38:41 +0900

    fix : 툴팁 UI 수정 - sm이상 md이하 css가 제대로 작동하지 않는 부분 수정

--- src/components/common/Tooltip.tsx
+++ src/components/common/Tooltip.tsx
@@ -10,15 +10,17 @@ interface TooltipProps {
 const Tooltip = ({ title, infoText, idx, position }: TooltipProps) => {
   return (
     <div
-      className={`flex flex-col absolute mb-2 -ml-1 md:ml-0 text-start tooltip  max-w-[256px] w-auto  px-2 py-1 md:p-4 bg-gray-200 dark:bg-gray-600 text-gray-800 dark:text-white rounded-lg shadow-lg z-50 bottom-full left-1/2 transform md:-translate-x-1/2  ${
+      className={`flex flex-col absolute mb-2 -ml-1 md:ml-0 text-start tooltip  max-w-[256px] w-auto  px-2 py-1 md:p-4 bg-gray-200 dark:bg-gray-600 text-gray-800 dark:text-white rounded-lg shadow-lg z-50 bottom-full left-1/2 transform sm:-translate-x-1/2  ${
         position === 'left'
           ? (idx! + 1) % 3 === 0
             ? ' -translate-x-1/2'
-            : ' -translate-x-[20%] tooltip_left'
+            : (idx! + 1) % 5 === 1
+              ? '-translate-x-[20%] sm:-translate-x-[20%] tooltip_left_sm'
+              : ' -translate-x-[20%] tooltip_left'
           : (idx! + 1) % 3 === 1
             ? ' -translate-x-1/2'
             : (idx! + 1) % 5 === 0
-              ? '-translate-x-[80%] md:-translate-x-[80%] tooltip_right_md'
+              ? '-translate-x-[80%] sm:-translate-x-[80%] tooltip_right_sm'
               : ' -translate-x-[80%] tooltip_right'
       }`}
     >
