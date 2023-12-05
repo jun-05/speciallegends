6a8587254a0d0533bcc85e1ebfe8c7a5dd5bb0b0
Jun_53 <58911113+jun-05@users.noreply.github.com>
2023-12-05 14:39:53 +0900

    fix : 누락된 구글 애널리틱스설정 추가

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,6 +7,26 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <link rel="icon" href="/favicon.ico" />
+
+          {process.env.NODE_ENV === 'production' && (
+            <>
+              <script
+                async
+                src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+              />
+              <script
+                dangerouslySetInnerHTML={{
+                  __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
+              `,
+                }}
+              />
+            </>
+          )}
+
           <meta
             name="viewport"
             content="initial-scale=1.0, width=device-width"
